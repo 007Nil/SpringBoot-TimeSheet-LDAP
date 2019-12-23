c8393a386e8e4563874dc7a0b97a5ef406e0e5b3
nil <nil@nil-HP>
2019-12-24 02:44:42 +0530

    LDAP fetch complete using spring-ldap-core API

--- timesheetWebapp/src/main/java/com/example/dao/PersonDao.java
+++ timesheetWebapp/src/main/java/com/example/dao/PersonDao.java
@@ -2,39 +2,148 @@ package com.example.dao;
 
 public class PersonDao {
 
-    private String fullName;
-    private String lastName;
+	private String fullName;
+	private String lastName;
+	private String userName;
+	private String description;
+	private String mobile;
+	private String street;
+	private String postOfficeBox;
+	private String postalCode;
+	private String postalAddress;
+	private String employeeType;
+	private String Natinality;
+	private String state;
+	private String organigation;
+	private String email;
 
-   public PersonDao() {
-	// TODO Auto-generated constructor stub
-}
+	public PersonDao() {
+		// TODO Auto-generated constructor stub
+	}
 
-    public PersonDao(String fullName, String lastName) {
-        this.fullName = fullName;
-        this.lastName = lastName;
-    }
+	public PersonDao(String fullName, String lastName) {
+		this.fullName = fullName;
+		this.lastName = lastName;
+	}
 
-    public String getFullName() {
-        return fullName;
-    }
+	public String getFullName() {
+		return fullName;
+	}
 
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
 
-    public String getLastName() {
-        return lastName;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-    @Override
-    public String toString() {
-        return "Person{" +
-                "fullName='" + fullName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                '}';
-    }
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getPostOfficeBox() {
+		return postOfficeBox;
+	}
+
+	public void setPostOfficeBox(String postOfficeBox) {
+		this.postOfficeBox = postOfficeBox;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public String getPostalAddress() {
+		return postalAddress;
+	}
+
+	public void setPostalAddress(String postalAddress) {
+		this.postalAddress = postalAddress;
+	}
+
+	public String getEmployeeType() {
+		return employeeType;
+	}
+
+	public void setEmployeeType(String employeeType) {
+		this.employeeType = employeeType;
+	}
+
+	public String getNatinality() {
+		return Natinality;
+	}
+
+	public void setNatinality(String natinality) {
+		Natinality = natinality;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getOrganigation() {
+		return organigation;
+	}
+
+	public void setOrganigation(String organigation) {
+		this.organigation = organigation;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "PersonDao [fullName=" + fullName + ", lastName=" + lastName + ", userName=" + userName
+				+ ", description=" + description + ", mobile=" + mobile + ", street=" + street + ", postOfficeBox="
+				+ postOfficeBox + ", postalCode=" + postalCode + ", postalAddress=" + postalAddress + ", employeeType="
+				+ employeeType + ", Natinality=" + Natinality + ", state=" + state + ", organigation=" + organigation
+				+ ", email=" + email + "]";
+	}
 }
--- timesheetWebapp/src/main/java/com/example/services/PersonService.java
+++ timesheetWebapp/src/main/java/com/example/services/PersonService.java
@@ -3,7 +3,6 @@ package com.example.services;
 import java.util.List;
 
 import javax.naming.NamingException;
-import javax.naming.directory.Attribute;
 import javax.naming.directory.Attributes;
 import javax.naming.directory.SearchControls;
 
@@ -17,7 +16,6 @@ import org.springframework.stereotype.Service;
 
 import com.example.dao.PersonDao;
 
-
 @Service
 public class PersonService {
 	private static final Integer THREE_SECONDS = 3000;
@@ -27,29 +25,41 @@ public class PersonService {
 
 	public List<PersonDao> getPersonNamesByLastName(String lastName) {
 		SearchControls sc = new SearchControls();
-        sc.setSearchScope(SearchControls.SUBTREE_SCOPE);
-        sc.setTimeLimit(THREE_SECONDS);
-        sc.setCountLimit(10);
-        sc.setReturningAttributes(new String[]{"cn", "givenName", "sn"});
+		sc.setSearchScope(SearchControls.SUBTREE_SCOPE);
+		sc.setTimeLimit(THREE_SECONDS);
+		sc.setCountLimit(10);
+		sc.setReturningAttributes(new String[] { "uid", "givenName", "sn", "description", "mail", "telephoneNumber",
+				"postOfficeBox", "postalCode", "postalAddress", "street", "employeeType", "l", "st", "o" });
 
-        AndFilter filter = new AndFilter();
-        filter.and(new EqualsFilter("objectclass", "person"));
-        filter.and(new EqualsFilter("cn", lastName));
+		AndFilter filter = new AndFilter();
+		filter.and(new EqualsFilter("objectclass", "person"));
+		filter.and(new EqualsFilter("cn", lastName));
 
-        return ldapTemplate.search(LdapUtils.emptyLdapName(), filter.encode(), sc, new PersonAttributesMapper());
+		return ldapTemplate.search(LdapUtils.emptyLdapName(), filter.encode(), sc, new PersonAttributesMapper());
 	}
+
 	/**
 	 * Custom person attributes mapper, maps the attributes to the person POJO
 	 */
 	private class PersonAttributesMapper implements AttributesMapper<PersonDao> {
 		public PersonDao mapFromAttributes(Attributes attrs) throws NamingException {
 			PersonDao person = new PersonDao();
-			person.setFullName( (String)attrs.get("givenName").get() );
 
-			Attribute sn = attrs.get("sn");
-			if (sn != null) {
-				person.setLastName((String) sn.get());
-			}
+			person.setFullName((String) attrs.get("givenName").get());
+			person.setLastName((String) attrs.get("sn").get());
+			person.setUserName((String) attrs.get("uid").get());
+			person.setDescription((String) attrs.get("description").get());
+			person.setMobile((String) attrs.get("telephoneNumber").get());
+			person.setStreet((String) attrs.get("street").get());
+			person.setPostOfficeBox((String) attrs.get("postOfficeBox").get());
+			person.setPostalCode((String) attrs.get("postalCode").get());
+			person.setPostalAddress((String) attrs.get("postalAddress").get());
+			person.setEmployeeType((String) attrs.get("employeeType").get());
+			person.setNatinality((String) attrs.get("l").get());
+			person.setState((String) attrs.get("st").get());
+			person.setOrganigation((String) attrs.get("o").get());
+			person.setEmail((String) attrs.get("mail").get());
+
 			return person;
 		}
 	}
