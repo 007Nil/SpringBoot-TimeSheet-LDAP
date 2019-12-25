828713c70eb1efc59da367be361ceab271019203
nil <nil@nil-HP>
2019-12-26 01:52:25 +0530

    Ajax Call finally works and it generate a dynamic table which contains all user details

--- timesheetWebapp/src/main/webapp/JS/home.js
+++ timesheetWebapp/src/main/webapp/JS/home.js
@@ -1,3 +1,8 @@
+/*
+Author : Sagnik Sarkar
+Date   : 26.12.19
+*/
+
 $(document).ready(function () {
     $.ajax({
         type: "GET",
@@ -5,12 +10,33 @@ $(document).ready(function () {
         cache: false,
 
         success: function (response) {
-            console.log(response);
+            var userData = JSON.parse(response);
             console.log('success');
+            const userAttributes = ['fullName', 'lastName', 'userName', 'description', 'mobile', 'street', 'postOfficeBox',
+                'postalCode', 'postalAddress', 'natinality', 'state', 'organigation', 'email'];
+
+            const userDetails = ['Full Name', 'Last Name', 'User Name', 'Description', 'Mobile', 'Street', 'Post Office',
+                'Postal Code', 'Postal Address', 'Natinality', 'State', 'Organigation', 'Email ID'];
+
+            const tableBody = $('#table-data');
+            var dataHtml = '';
+
+            // Finally works
+            //Dynamic Table which contains all user data fetch from ldap server which is store
+            //in a JESON Object
+            for (let user of userData) {
+                for (let i = 0; i < userAttributes.length; i++) {
+                    dataHtml += `<tr>
+                                 <td>${userDetails[i]}</td>
+                                 <td>${user[userAttributes[i]]}</td> 
+                                 </tr>`;
+                }
+            }
+
+            tableBody.append(dataHtml);
         },
         error: function () {
             console.log('An error occurred.');
         },
-
     });
 });
